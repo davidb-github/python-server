9aa76d4846aaae7c4b93a7f0f99ad728cc09a8fc
David Bing <davidb.devops@gmail.com>
2021-01-13 14:38:39 -0600

    update class: HandleRequests - implement and wire-up and update_location method

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from locations.request import get_all_locations, get_single_location,create_location, delete_location
+from locations.request import get_all_locations, get_single_location,create_location, delete_location, update_location
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from employees import get_all_employees, get_single_employee,create_employee, delete_employee
 from customers import get_all_customers,get_single_customer,create_customer, delete_customer
@@ -132,6 +132,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Delete a single animal from the list
         if resource == "animals":
             update_animal(id, post_body)
+        elif resource == "locations":
+            update_location(id, post_body)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
