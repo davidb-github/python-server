97705de71b0ac868a69061f08d019393e45f1c55
David Bing <davidb.devops@gmail.com>
2021-01-20 11:30:30 -0600

    update request_handler.py: import get_customers_by_email and fix response = f"{get_customers_by_email(value)}

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from locations.request import get_all_locations, get_single_location,create_location, delete_location, update_location
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from employees import get_all_employees, get_single_employee,create_employee, delete_employee, update_employee
-from customers import get_all_customers,get_single_customer,create_customer, delete_customer, update_customer
+from customers import get_all_customers,get_single_customer,get_customers_by_email, create_customer, delete_customer, update_customer
 import json
  
 # Here's a class. It inherits from another class.
@@ -154,7 +154,9 @@ class HandleRequests(BaseHTTPRequestHandler):
             # query parameter that specified the customer
             # email as a filtering value?
             if key == "email" and resource == "customers":
-                response = get_customers_by_email(value)
+                #response = get_customers_by_email(value)
+                response = f"{get_customers_by_email(value)}"
+                
 
         self.wfile.write(response.encode())
 
